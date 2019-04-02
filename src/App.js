371945d6b4a8a7740bafe3368cf2ee69bf5e067a
Benoît Hubert <benoithubert@gmail.com>
2019-04-02 10:11:24 +0200

    Split template into component hierarchy

--- src/App.js
+++ src/App.js
@@ -1,84 +1,40 @@
 import React, { Component, Fragment } from 'react';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import ArticleCard from './components/ArticleCard';
+import articleList from './articles';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-      <div className="container">
-      <header className="blog-header py-3">
-        <div className="row flex-nowrap justify-content-between align-items-center">
-          <div className="col-4 pt-1">
-            <a className="text-muted" href="#">Subscribe</a>
-          </div>
-          <div className="col-4 text-center">
-            <a className="blog-header-logo text-dark" href="#">Large</a>
-          </div>
-          <div className="col-4 d-flex justify-content-end align-items-center">
-            <a className="text-muted" href="#">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="mx-3" role="img" viewBox="0 0 24 24" focusable="false"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>
-            </a>
-            <a className="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
-          </div>
-        </div>
-      </header>
+        <div className="container">
+          <Header />
+          <Navbar />
     
-      <div className="nav-scroller py-1 mb-2">
-        <nav className="nav d-flex justify-content-between">
-          <a className="p-2 text-muted" href="#">World</a>
-          <a className="p-2 text-muted" href="#">U.S.</a>
-          <a className="p-2 text-muted" href="#">Technology</a>
-          <a className="p-2 text-muted" href="#">Design</a>
-          <a className="p-2 text-muted" href="#">Culture</a>
-          <a className="p-2 text-muted" href="#">Business</a>
-          <a className="p-2 text-muted" href="#">Politics</a>
-          <a className="p-2 text-muted" href="#">Opinion</a>
-          <a className="p-2 text-muted" href="#">Science</a>
-          <a className="p-2 text-muted" href="#">Health</a>
-          <a className="p-2 text-muted" href="#">Style</a>
-          <a className="p-2 text-muted" href="#">Travel</a>
-        </nav>
-      </div>
-    
-      <div className="jumbotron p-4 p-md-5 text-white rounded bg-dark">
-        <div className="col-md-6 px-0">
-          <h1 className="display-4 font-italic">Title of a longer featured blog post</h1>
-          <p className="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
-          <p className="lead mb-0"><a href="#" className="text-white font-weight-bold">Continue reading...</a></p>
-        </div>
-      </div>
-    
-      <div className="row mb-2">
-        <div className="col-md-6">
-          <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div className="col p-4 d-flex flex-column position-static">
-              <strong className="d-inline-block mb-2 text-primary">World</strong>
-              <h3 className="mb-0">Featured post</h3>
-              <div className="mb-1 text-muted">Nov 12</div>
-              <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" className="stretched-link">Continue reading</a>
-            </div>
-            <div className="col-auto d-none d-lg-block">
-              <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+          <div className="jumbotron p-4 p-md-5 text-white rounded bg-dark">
+            <div className="col-md-6 px-0">
+              <h1 className="display-4 font-italic">Title of a longer featured blog post</h1>
+              <p className="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
+              <p className="lead mb-0"><a href="#" className="text-white font-weight-bold">Continue reading...</a></p>
             </div>
           </div>
-        </div>
-        <div className="col-md-6">
-          <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div className="col p-4 d-flex flex-column position-static">
-              <strong className="d-inline-block mb-2 text-success">Design</strong>
-              <h3 className="mb-0">Post title</h3>
-              <div className="mb-1 text-muted">Nov 11</div>
-              <p className="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" className="stretched-link">Continue reading</a>
-            </div>
-            <div className="col-auto d-none d-lg-block">
-              <svg className="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            </div>
+            
+          <div className="row mb-2">
+            {
+              articleList.map((singleArticle) => (
+                <ArticleCard
+                  key={singleArticle.id}
+                  category={singleArticle.category}
+                  title={singleArticle.title}
+                  date={singleArticle.date}
+                  excerpt={singleArticle.excerpt}
+                />
+              ))
+            }
           </div>
         </div>
-      </div>
-    </div>
     
     <main role="main" className="container">
       <div className="row">
